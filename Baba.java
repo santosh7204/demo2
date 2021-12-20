24d1bf7057a237f1e08028a7c0c6fc1e4f40cc70
santosh <santuyanagunti23.xworkz@gmail.com>
2021-12-20 21:52:40 +0530

    baba programme

---
+++ Baba.java
@@ -0,0 +1,16 @@
+class Baba{
+  public static void main(String[] args){
+	  boolean[] god={true,false,true,false,true,false,true, true,false,true,true};
+	  System.out.println(god[0]);
+      System.out.println(god[1]);
+      System.out.println(god[2]);
+	  System.out.println(god[3]); 
+	  System.out.println(god[4]);
+      System.out.println(god[5]);
+	  System.out.println(god[6]);
+	  System.out.println(god[7]);
+	  System.out.println(god[8]);
+	  System.out.println(god[9]);
+      System.out.println(god[10]);
+  }
+}
